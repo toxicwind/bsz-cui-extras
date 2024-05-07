1398cb87290d563f82cff65662f46d8fe6f172d6
Beinsezii <beinsezii@gmail.com>
2024-05-07 15:05:59 -0700

    Fix python 3.12

--- __init__.py
+++ __init__.py
@@ -4,9 +4,13 @@ Forwards the contents of ./bsz-nodes/ for reading when the whole repo is cloned 
 Should make it compatible with https://github.com/ltdrdata/ComfyUI-Manager
 """
 
-import importlib
-loader = importlib.find_loader('bsz-nodes', __path__)
-mod = loader.load_module()
+from importlib import machinery as ilm
+from importlib import util as ilu
+
+finder = ilm.PathFinder()
+spec = finder.find_spec("bsz-nodes", __path__)
+mod = ilu.module_from_spec(spec)
+spec.loader.exec_module(mod)
 
 NODE_CLASS_MAPPINGS = mod.NODE_CLASS_MAPPINGS
 NODE_DISPLAY_NAME_MAPPINGS = mod.NODE_DISPLAY_NAME_MAPPINGS
